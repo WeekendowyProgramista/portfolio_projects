fc7e65a8d4428001dd6c4833d27292a48cd15032
WeekendowyProgramista <zbr2183522@gmail.com>
2026-07-12 12:07:49 +0200

    child sample added

--- school_kids_organizer/main.py
+++ school_kids_organizer/main.py
@@ -30,7 +30,7 @@ class Group:
 
     def add_child(self, child: Child):
         """
-        Function return groups limit
+        Function return group limit True/False
         :param child: number of children
         :return: in or out of limit
         """
@@ -47,6 +47,30 @@ class Kindergarden:
     def __init__(self):
         self.group = []
 
-    def add_group(self):
+    def take_group(self, group:Group):
+        """
+        Function return groups
+        :param group: class Group
+        :return: self.group []
+        """
+        self.group.append(group)
+
+    def group_organizer(self):
         pass
+
+child_1 = Child('Daniel','Żebrowski', 'Witosa 10', 'Male', '1', '0-3')
+child_2 = Child('Adrianna','Wilczewska', 'Władysława 4', 'Female', '4', '3-6')
+
+print(child_1.first_name, child_1.last_name, child_1.address, child_1.gender, child_1.age, child_1.group)
+print(child_2.first_name, child_2.last_name, child_2.address, child_2.gender, child_2.age, child_2.group)
+
+group_0_3 = Group(group_name='0-3', min_age=0, max_age=3, limit=12)
+group_3_6 = Group(group_name='3-6', min_age=3, max_age=6, limit=22)
+
+print(group_0_3.group_name, group_0_3.min_age, group_0_3.max_age, group_0_3.limit)
+print(group_3_6.group_name, group_3_6.min_age, group_3_6.max_age, group_3_6.limit)
+
+add_group_0_3 = Group.add_child(self=group_0_3, child=child_1)
+add_group_3_6 = Group.add_child(self=group_3_6, child=child_2)
+
 
