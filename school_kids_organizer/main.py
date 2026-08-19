75c6b93214135135ab7ae0229b1b2bf20bcc2ac4
WeekendowyProgramista <zbr2183522@gmail.com>
2026-08-19 18:59:49 +0200

    fake db add

--- school_kids_organizer/main.py
+++ school_kids_organizer/main.py
@@ -1,18 +1,4 @@
 
-"""
-Tworzę grupę
-↓
-Dodaję jedno dziecko
-↓
-Wyświetlam grupę
-↓
-Czy dziecko jest w środku?
-
-- usuwam def show info na rzecz __str__(...) self.children
-- add_child zmieniłem na set_limit, bardziej pasuje
-"""
-
-
 class Child:
     """
     stores a child's data
@@ -41,7 +27,7 @@ class Group:
     def __str__(self):
         return f'{self.group_name}, {self.min_age}, {self.max_age}, {self.limit}, {self.children}'
 
-    def set_limit(self, child: Child):
+    def add_child(self, child: Child):
         """
         Function return group limit True/False
         :param child: number of children
@@ -53,29 +39,45 @@ class Group:
             self.children.append(child)
             return True
 
-class Kindergarden:
+class Kindergarten:
     """
     contains all groups, assigns children to the appropriate groups, and transfers children between groups
     """
     def __init__(self):
-        self.groups = []
+        self.all_children = []
+        self.all_groups = []
 
     def __str__(self):
-        return f'All groups: {self.groups}'
+        groups = []
+        for group in self.all_groups:
+            groups.append(str(group))
+        groups_final = '\n'.join(groups)
 
-    def get_groups(self, group: Group):
-        return self.groups.append(group)
+        children = []
+        for child in self.all_children:
+            children.append(str(child))
+        children_final = '\n'.join(children)
+
+        return f'{children_final}\n{groups_final}'
+
+    def add_groups(self, group: Group):
+        self.all_groups.append(group)
+
+    def add_children(self, child: Child):
+        self.all_children.append(child)
 
 
 child_1 = Child('Daniel','Żebrowski', 'Witosa 10', 'Male', 1)
 child_2 = Child('Adrianna','Wilczewska', 'Władysława 4', 'Female', 4)
 
-group_0_3 = Group(group_name='0_3',min_age= 0, max_age= 3, limit= 12)
-group_3_6 = Group(group_name='3_6',min_age= 3, max_age= 6, limit= 22)
+group_0_3 = Group(group_name='smoczki',min_age= 0, max_age= 3, limit= 12)
+group_3_6 = Group(group_name='patyczki',min_age= 3, max_age= 6, limit= 22)
 
-group_0_3.set_limit(child=child_1)
-group_3_6.set_limit(child=child_2)
+group_0_3.add_child(child=child_1)
+group_3_6.add_child(child=child_2)
 
-Kindergarden().get_groups(group=group_0_3)
-Kindergarden().get_groups(group=group_3_6)
-print(Kindergarden())
+kindergarten = Kindergarten()
+kindergarten.add_groups(group=group_3_6)
+kindergarten.add_groups(group=group_0_3)
+kindergarten.add_children(child=child_1)
+kindergarten.add_children(child=child_2)
